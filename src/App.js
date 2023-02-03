860266c08fc399a6599e57c70840fb7eb970a06c
Molnar-Balazs-Imre <molnarbalazsimre@gmail.com>
2023-02-04 00:44:14 +0200

    Add a new feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './app.scss';
 import Calculator from './Calculator/Calculator';
+import Todo from './Todo/Todo';
 
 function App() {
   return ( 
     <>
        <div>
         <Calculator/>
+        <Todo/>
        </div>
     </>
     
---
+++ src/Todo/Todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Todo.scss';
+
+function Todo() {
+  return (
+    <div>Todo</div>
+  )
+}
+
+export default Todo
